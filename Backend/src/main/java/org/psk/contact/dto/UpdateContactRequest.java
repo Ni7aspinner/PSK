2363c4ccd444ce43678d82d19e455321aed32af8
SteponasDominas <steponasdom@gmail.com>
2026-05-14 19:16:18 +0300

    minor fixes

--- Backend/src/main/java/org/psk/contact/dto/UpdateContactRequest.java
+++ Backend/src/main/java/org/psk/contact/dto/UpdateContactRequest.java
@@ -1,35 +1,12 @@
 package org.psk.contact.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
-public class UpdateContactRequest {
-
-  @NotBlank
-  @Size(max = 100)
-  private String firstName;
-
-  @NotBlank
-  @Size(max = 100)
-  private String lastName;
-
-  @Size(max = 150)
-  private String position;
-
-  @Email
-  @Size(max = 255)
-  private String email;
-
-  @Size(max = 50)
-  private String phone;
-
-  private boolean primary;
-
-  @NotNull private Long supplierId;
+@EqualsAndHashCode(callSuper = true)
+public class UpdateContactRequest extends CreateContactRequest {
 
   @NotNull private Long version;
 }
