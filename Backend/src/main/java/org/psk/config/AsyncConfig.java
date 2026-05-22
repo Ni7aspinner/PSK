c777203ddc6c722251e8770e778a1c49622d21ef
Ni7aspinner <167442317+Ni7aspinner@users.noreply.github.com>
2026-05-22 15:45:09 +0300

    feature/pr8 spotless applied

--- Backend/src/main/java/org/psk/config/AsyncConfig.java
+++ Backend/src/main/java/org/psk/config/AsyncConfig.java
@@ -1,7 +1,6 @@
 package org.psk.config;
 
 import java.util.concurrent.ThreadPoolExecutor;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
