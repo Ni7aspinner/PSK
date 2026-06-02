e73e9c53c51da1b53bff36ea5c9a4dcc2a7a1cfb
IceHawkey9588 <nojus.cininas@mif.stud.vu.lt>
2026-06-02 22:27:35 +0300

    remove permitAll()

--- Backend/src/main/java/org/psk/security/config/SecurityConfig.java
+++ Backend/src/main/java/org/psk/security/config/SecurityConfig.java
@@ -44,12 +44,10 @@ public class SecurityConfig {
                     .permitAll()
                     .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
                     .permitAll()
-                    .requestMatchers("/reports/active-suppliers/pdf")
-                    .permitAll()
                     .requestMatchers("/admin/**")
                     .hasRole("ADMIN")
                     .requestMatchers("/reports/**")
-                    .hasAnyRole("USER", "ADMIN")
+                    .hasAnyRole("ADMIN")
                     .anyRequest()
                     .authenticated())
         .exceptionHandling(
